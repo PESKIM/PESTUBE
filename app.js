a81d7b1b3d08205e798f539be29b2e36e0da56f7
PES <21ckpg@gmail.com>
2019-03-22 15:26:39 +0900

    20190322

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+const app = express();
+
+app.set('view engine', "pug")
+app.use(cookieParser()); // cookie를 전달받아 사용할수있는 미들웨어
+app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어
+app.use(bodyParser.urlencoded({ extended: true })); // request 정보에서 form이나 json 형태로 된 body를 검사함 아바타의 사진이나 비디오를 업로드 할 때, 제목이나 댓글 같은 정보를 전달할떄 form에 담아 전달하기 떄문 
+app.use(helmet()); // 보안 미들웨어
+app.use(morgan("dev")); // 앱에서 발생하는 모든 일들을 logging 하는 역할
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
